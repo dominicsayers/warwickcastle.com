b49437010c7fb9098c193d4a6ff7aa1d476d6c9f
Dominic Sayers <dominic@sayers.cc>
2012-05-28 12:27:40 +0100

    Refinery CMS created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarwickcastleCom::Application.config.secret_token = '2d38df57c10cc173b8c85057b0aa3b09af7e85920b4b3eeff28794dc25323267f0b36a92c4aa155813e1041a32c7c88f7da39af15bd9187d3dc95969a572bc10'
